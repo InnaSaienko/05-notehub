d4a12aeb158b58819b72a22dad1be10890df33f3
Inna Saienko <inna.saienko.nl@gmail.com>
2026-08-12 09:47:19 +0200

    feat: add NodeList component for notes displaying

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,5 +1,38 @@
-const NoteList = () => {
-  return "NoteList";
+import type {Note} from "../../types/note.ts";
+import css from "./NoteList.module.css"
+import {useState} from "react";
+import {useMutation} from "@tanstack/react-query";
+import {deleteNote} from "../../services/noteService.ts";
+
+interface NoteListProps {
+    notes: Note[];
+}
+const NoteList = ({notes}: NoteListProps) => {
+    const [deletingId, setDeletingId] = useState<string | null>(null);
+
+    const deleteNoteMutation = useMutation({
+        mutationFn: deleteNote,
+        onMutate: (id: string) => setDeletingId(id),
+    });
+
+    return <ul className={css.list}>
+        {notes.map(note => (
+            <li key={note.id} className={css.listItem}>
+                <h2 className={css.title}>{note.title}</h2>
+                <p className={css.content}>{note.content}</p>
+                <div className={css.footer}>
+                    <span className={css.tag}>{note.tag}</span>
+                    <button className={css.button}
+                            disabled={deletingId === note.id}
+                            onClick={() => deleteNoteMutation.mutate(note.id)}
+                    >
+                        Delete
+                    </button>
+                </div>
+            </li>
+        ))}
+    </ul>
+        ;
 };
 
 export default NoteList;
