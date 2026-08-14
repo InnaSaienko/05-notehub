48fb170ecd2a32bc5b0c871e5a25745721ffca12
Inna Saienko <inna.saienko.nl@gmail.com>
2026-08-14 09:34:01 +0200

    feat(App): assign callback function on NoteForm

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -55,7 +55,7 @@ const App = () => {
             </main>
             {isModalOpen && (
                 <Modal onClose={() => setIsModalOpen(false)}>
-                    <NoteForm/>
+                    <NoteForm onSuccessSubmit={() => setIsModalOpen(false)}/>
                 </Modal>
             )}
         </div>
